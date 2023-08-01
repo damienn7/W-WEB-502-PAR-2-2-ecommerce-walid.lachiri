9dac69a80725fcd72441da628e12084aa1e96d87
AlexEl972 <alexendre.elie@epitech.eu>
2023-08-01 09:59:05 +0200

    [FIX] changement en api

--- Ecommerce/server/routes/api.php
+++ Ecommerce/server/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// CREATE
+Route::post('users/', [ArticleController::class, 'createUser']);
+// --------------------------------
+
+// READ 
+Route::get('users', [ArticleController::class, 'index']);
+Route::get('users/{id}', [ArticleController::class, 'show']);
+// --------------------------------
+
+// UPDATE 
+Route::put('users/{id}', [ArticleController::class, 'update']);
+// --------------------------------
+
+
+// Delete 
+Route::delete('users/{id}/', [ArticleController::class, 'destroy']);
+// --------------------------------
+
+
+
+
