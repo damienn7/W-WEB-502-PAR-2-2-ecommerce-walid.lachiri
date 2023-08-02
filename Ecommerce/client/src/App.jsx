ff1d41f79327f8a387ccd2663a3fc015c449227d
DzStylDev <amine.meddour@epitech.eu>
2023-08-02 15:27:40 +0200

    multiple errors

---
+++ Ecommerce/client/src/App.jsx
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { Breadcrumbs } from '@mui/material';
+import { Route, useRoutes } from 'react-router-dom';
+import Accueil from './Accueil';
+import { useLocation } from "react-router-dom";
+
+function App() {
+  const location = useLocation();
+
+    let element = useRoutes([
+        {
+          path: "/",
+          element: <Accueil />,
+          children: [
+            {
+              path: location.pathname,
+              element: <Accueil />,
+            },
+            // { 
+            //     path: "tasks", 
+            //     element: <DashboardTasks /> 
+            // },
+          ],
+        },
+        {
+          path: location.pathname !== 'model' ? '/' : '/model',
+          element: < Breadcrumbs />
+        }
+      ]);
+
+      return element;
+}
+
+export default App;
