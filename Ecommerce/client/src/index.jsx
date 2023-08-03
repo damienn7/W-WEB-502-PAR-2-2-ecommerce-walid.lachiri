03692c8c36129f8898dbf8ef1522957b308354cc
thivco <thibault.vivier@epitech.eu>
2023-08-03 10:48:41 +0200

    [rename] react files and structure

--- Ecommerce/client/src/index.jsx
+++ Ecommerce/client/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 // import ArticlesView from './Articles';
-import Unique from './Unique'
-import Accueil from './Accueil'
+import Unique from './components/Unique'
+import Accueil from './components/Accueil'
 import { BrowserRouter } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 
