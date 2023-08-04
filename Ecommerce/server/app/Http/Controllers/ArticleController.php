47cfd81b618f9c6f93aaa575efd8790ce02945df
Mehdi <mehdi1.zenati@epitech.eu>
2023-08-04 15:18:52 +0200

    ItemPopularity done

--- Ecommerce/server/app/Http/Controllers/ArticleController.php
+++ Ecommerce/server/app/Http/Controllers/ArticleController.php
@@ -18,6 +18,7 @@ public function METHODEDEFILSDEPUTE(Request $request){
     return DB::table('items')       
             ->join('categories', 'categories.id', '=', 'items.id_category')
             // ->join('ratings', 'ratings.id_article', '=', 'items.id' )
+            ->orderBy('views', 'desc')
             ->get();
 }
 
