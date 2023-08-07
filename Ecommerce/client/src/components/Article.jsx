2506774e73e7ec9519b296ecf4448a470ce322c4
Mehdi <mehdi1.zenati@epitech.eu>
2023-08-07 12:06:57 +0200

    Now I have become Death, the destroyer of worlds.

--- Ecommerce/client/src/components/Article.jsx
+++ Ecommerce/client/src/components/Article.jsx
@@ -1,15 +1,21 @@
+import { faDisplay } from "@fortawesome/free-solid-svg-icons";
 import { Container, Card, TableCell, TableRow } from "@mui/material";
 
 export default function Article({ article }) {
   return article.map((article, index) => (
-    <div key={index} style={{backgroundColor: "blue", margin: '0 auto'}} >
-        <div style={{ backgroundColor: 'bluecyan', border: '1px solid black', padding: '1em'}}>
+    <div key={index} style={{ margin: '2 auto'}} >
+        <div style={{border: '1px solid black', padding: '1em',}}>
+          <div style={{display:"flex"}}>
+          <img src={article.image} width={150} />
+          <div style={{display:"blocks"}}>
+
           <h1 style={{ fontSize: '1.4em'}}>{article.name}</h1>
+          <h5>Prix : {article.price}€</h5>
           <h2 style={{ fontSize: '1em'}}>{article.description}</h2>
-          <img src={article.image} width={300} />
-          <h4>{article.views}</h4>
-          <h5>{article.price}</h5>
-          <h5>{article.stock}</h5>
+          </div>
+          </div>
+          {/* <h4>{article.views}</h4> */}
+          {/* <h5>{article.stock}</h5> */}
         </div>
     </div>
   ));
