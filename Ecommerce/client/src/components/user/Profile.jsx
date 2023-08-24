5d502f681bf2548d276b0ea92d5141b4ca45216e
AlexEl972 <alexendre.elie@epitech.eu>
2023-08-24 10:13:35 +0200

    [FIX] edit user

--- Ecommerce/client/src/components/user/Profile.jsx
+++ Ecommerce/client/src/components/user/Profile.jsx
@@ -3,26 +3,36 @@ import React, { useState, useEffect } from "react";
 
 const Profile = () => {
   const id = localStorage.getItem("id");
-  axios.get(`http://localhost:8000/api/users/${user.id}`).then((response) => {
-    const name = response.data.name;
-    const mail = response.data.mail;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+  const [formData, setFormData] = useState({
+    name: '',
+    mail: '',
+    new: '',
   });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.put(`http://localhost:8000/api/users/${id}`,formData)
+    .then((response) => {
+      console.log('Utilisateur mis à jour:', response.data);
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      console.error('Erreur lors de la mise à jour de l\'utilisateur:', error);
+    });
+  };
   return (
   <div>
-    <h2>Inscription</h2>
-    <form /*onSubmit={handleSubmit}*/>
+    <h2>Modifier mes informations</h2>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name:</label>
-      <input type="text" name="name" value={name} /*onChange={handleChange}*/ />
-
+      <input type="text" name="name" onChange={handleChange} />
       <label htmlFor="mail">mail:</label>
-      <input type="email" name="mail" value={mail} /*onChange={handleChange}*//>
-
-      <label htmlFor="password">Mot de passe:</label>
-      <input type="password" name="password" value="password" />
-      <label htmlFor="cpassword">Confirmez le mot de passe:</label>
-      <input type="password" name="cpassword" value="password" />
+      <input type="email" name="mail" onChange={handleChange}/>
       <label htmlFor="new">Nouveau mot de passe:</label>
-      <input type="password" name="new" value="password" />
+      <input type="password" name="new"/>
       <button type="submit">Inscription</button>
     </form>
   </div>
