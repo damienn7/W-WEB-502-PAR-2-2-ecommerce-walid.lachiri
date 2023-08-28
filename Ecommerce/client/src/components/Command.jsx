158895ad1c984e990d7aaf73baf705a97f323bd4
AlexEl972 <alexendre.elie@epitech.eu>
2023-08-28 09:49:33 +0200

    test

--- Ecommerce/client/src/components/Command.jsx
+++ Ecommerce/client/src/components/Command.jsx
@@ -1,85 +1,79 @@
-import React, { useState } from "react";
-import axios from "axios";
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
+// // import React from 'react';
+// // import { withStyles, makeStyles } from '@material-ui/core/styles';
 
-function createData(order_date, delivery_address, id, total, image,nom) {
-    return { order_date, delivery_address, id, total, image,nom };
-  }
-  const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
-  ];
-  function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-      return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-      return 1;
-    }
-    return 0;
-  }
-  function getComparator(order, orderBy) {
-    return order === 'desc'
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
-  }
-  
-  function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-      const order = comparator(a[0], b[0]);
-      if (order !== 0) return order;
-      return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-  }
-  const headCells = [
-    { id: 'order_date', numeric: false, disablePadding: true, label: 'order_date' },
-    { id: 'delivery_address', numeric: true, disablePadding: false, label: 'delivery_address' },
-    { id: 'id', numeric: true, disablePadding: false, label: 'id' },
-    { id: 'total', numeric: true, disablePadding: false, label: 'total' },
-    { id: 'image', numeric: true, disablePadding: false, label: 'image' },
-    { id: 'nom', numeric: true, disablePadding: false, label: '' },
-    
-  ];
-const Command = () => {
-    const id = localStorage.getItem("id");
-    axios.get(`http://127.0.0.1:8000/api/order/user/${id}`)
-        .then((response) => {
-          console.log(response.data) ;
-        })
-};
+// // import Table from '@material-ui/core/Table';
+// // import TableBody from '@material-ui/core/TableBody';
+// // import TableCell from '@material-ui/core/TableCell';
+// // import TableContainer from '@material-ui/core/TableContainer';
+// // import TableHead from '@material-ui/core/TableHead';
+// // import TableRow from '@material-ui/core/TableRow';
+// // import Paper from '@material-ui/core/Paper';
 
-export default Command;
+// const StyledTableCell = withStyles((theme) => ({
+//   head: {
+//     backgroundColor: theme.palette.common.black,
+//     color: theme.palette.common.white,
+//   },
+//   body: {
+//     fontSize: 14,
+//   },
+// }))(TableCell);
+
+// const StyledTableRow = withStyles((theme) => ({
+//   root: {
+//     '&:nth-of-type(odd)': {
+//       backgroundColor: theme.palette.action.hover,
+//     },
+//   },
+// }))(TableRow);
+
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+// const useStyles = makeStyles({
+//   table: {
+//     minWidth: 700,
+//   },
+// });
+
+// export default function CustomizedTables() {
+//   const classes = useStyles();
+
+//   return (
+//     <TableContainer component={Paper}>
+//       <Table className={classes.table} aria-label="customized table">
+//         <TableHead>
+//           <TableRow>
+//             <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+//             <StyledTableCell align="right">Calories</StyledTableCell>
+//             <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+//             <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+//             <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {rows.map((row) => (
+//             <StyledTableRow key={row.name}>
+//               <StyledTableCell component="th" scope="row">
+//                 {row.name}
+//               </StyledTableCell>
+//               <StyledTableCell align="right">{row.calories}</StyledTableCell>
+//               <StyledTableCell align="right">{row.fat}</StyledTableCell>
+//               <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+//               <StyledTableCell align="right">{row.protein}</StyledTableCell>
+//             </StyledTableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </TableContainer>
+//   );
+// }
